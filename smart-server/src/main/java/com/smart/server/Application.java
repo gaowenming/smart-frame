dd31256b8163802e8a2f32755b5eda405d9fb902
gaowenming <gaowenming@maizicloud.com>
2017-05-20 19:10:58 +0800

    init

--- smart-server/src/main/java/com/smart/server/Application.java
+++ smart-server/src/main/java/com/smart/server/Application.java
@@ -2,14 +2,12 @@ package com.smart.server;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAutoConfiguration
 @SpringBootApplication
-@EnableTransactionManagement
+@EnableTransactionManagement // 开启事务
 @ComponentScan("com.smart") // 扫描service和controller
 @MapperScan("com.smart.mapper") // 扫描mapper
 public class Application {
